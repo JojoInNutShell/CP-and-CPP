918b395b28500318f5ba1d11f2a0ae2821a03009
MikiMiku <63213833+JojoInNutShell@users.noreply.github.com>
2023-03-03 16:47:10 +0700

    Add files via upload

---
+++ starter.cpp
@@ -0,0 +1,103 @@
+/*
+	Author: MikiMiku
+	
+	Observation:
+	
+	Idea: 
+		
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* DEBUGGING */
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+/* GLOBAL OVERWRITE */
+//#define int long long
+#define endl '\n'
+
+/* CONTAINER SHORTCUT */
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pb push_back 
+#define eb emplace_back 
+#define mp make_pair
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+  
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+/* BIT MANIPULATION */
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define LSB(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+/* GEOMETRY */
+typedef complex<double> Point;
+
+/* TYPE RE-DEFINE */
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+/* CONSTANT */
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const int FMOD = 998244353; 
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+const double PI=acos(-1);
+
+/* SHORTCUT */
+#define fi first
+#define se second
+typedef pair<int, int> ii;  
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef map<int,int> mii; 
+
+/* RANDOM */
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count()); 
+#define SHUF(v) shuffle(all(v), RNG); 
+
+
+//FLOAT PRECISION SETTINGS
+/*
+cout.setf(ios::fixed,ios::floatfield);
+cout.precision(3);
+*/
+//FILE IO
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+//........................................................................
+
+signed main() {
+	ios_base::sync_with_stdio(0);cin.tie(nullptr); cout.tie(nullptr);
+	
+	return 0;
+}
